58c4454222500bd811bc11e411d7ab41e56ae547
Daxmatic <dax.hoes@gmail.com>
2022-11-19 13:19:18 +0100

    reshuffle list

--- BPM2MS_v1/ContentView.swift
+++ BPM2MS_v1/ContentView.swift
@@ -35,26 +35,26 @@ struct ContentView: View {
                 Spacer()
                 List {
                     Text("2th S: \(NoteType.half(bpm: Double(bpms[value])).description)")
-                    Text("2th D: \(NoteType.halfDot(bpm: Double(bpms[value])).description)")
-                    Text("2th T: \(NoteType.halfTriplet(bpm: Double(bpms[value])).description)")
                     Text("4th S: \(NoteType.quarter(bpm: Double(bpms[value])).description)")
-                    Text("4th D: \(NoteType.quarterDot(bpm: Double(bpms[value])).description)")
-                    Text("4th T: \(NoteType.quarterTriplet(bpm: Double(bpms[value])).description)")
-                }
-                List {
                     Text("8th S: \(NoteType.eight(bpm: Double(bpms[value])).description)")
-                    Text("8th D: \(NoteType.eightDot(bpm: Double(bpms[value])).description)")
-                    Text("8th T: \(NoteType.eightTriplet(bpm: Double(bpms[value])).description)")
                     Text("16th S: \(NoteType.sixt(bpm: Double(bpms[value])).description)")
-                    Text("16th D: \(NoteType.sixtDot(bpm: Double(bpms[value])).description)")
-                    Text("16th T: \(NoteType.sixtTriplet(bpm: Double(bpms[value])).description)")
+                    Text("32th S: \(NoteType.thirtysecond(bpm: Double(bpms[value])).description)")
+                    Text("64th S: \(NoteType.sixtyfourth(bpm: Double(bpms[value])).description)")
                 }
                 List {
-                    Text("32th S: \(NoteType.thirtysecond(bpm: Double(bpms[value])).description)")
+                    Text("2th D: \(NoteType.halfDot(bpm: Double(bpms[value])).description)")
+                    Text("4th D: \(NoteType.quarterDot(bpm: Double(bpms[value])).description)")
+                    Text("8th D: \(NoteType.eightDot(bpm: Double(bpms[value])).description)")
+                    Text("16th D: \(NoteType.sixtDot(bpm: Double(bpms[value])).description)")
                     Text("32th D: \(NoteType.thirtysecondDot(bpm: Double(bpms[value])).description)")
-                    Text("32th T: \(NoteType.thirtysecondTriplet(bpm: Double(bpms[value])).description)")
-                    Text("64th S: \(NoteType.sixtyfourth(bpm: Double(bpms[value])).description)")
                     Text("64th D: \(NoteType.sixtyfourthDot(bpm: Double(bpms[value])).description)")
+                }
+                List {
+                    Text("2th T: \(NoteType.halfTriplet(bpm: Double(bpms[value])).description)")
+                    Text("4th T: \(NoteType.quarterTriplet(bpm: Double(bpms[value])).description)")
+                    Text("8th T: \(NoteType.eightTriplet(bpm: Double(bpms[value])).description)")
+                    Text("16th T: \(NoteType.sixtTriplet(bpm: Double(bpms[value])).description)")
+                    Text("32th T: \(NoteType.thirtysecondTriplet(bpm: Double(bpms[value])).description)")
                     Text("64th T: \(NoteType.sixtyfourthTriplet(bpm: Double(bpms[value])).description)")
                 }
             }
