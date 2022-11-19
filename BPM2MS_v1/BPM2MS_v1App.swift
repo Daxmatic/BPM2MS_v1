612dff59bee32dc42f4f687dd889d311ccbf0064
Daxmatic <dax.hoes@gmail.com>
2022-11-19 03:30:43 +0100

    Initial Commit

---
+++ BPM2MS_v1/BPM2MS_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  BPM2MS_v1App.swift
+//  BPM2MS_v1
+//
+//  Created by Dax Hoes on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BPM2MS_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
