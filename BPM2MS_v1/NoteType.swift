09af4080cd3d60684fb5ccbb57527b05d015fb1d
Daxmatic <dax.hoes@gmail.com>
2022-11-19 04:39:09 +0100

    added rudimentary note divisions

---
+++ BPM2MS_v1/NoteType.swift
@@ -0,0 +1,35 @@
+//
+//  NoteType.swift
+//  BPM2MS_v1
+//
+//  Created by Dax Hoes on 19/11/22.
+//
+
+import Foundation
+
+enum NoteType {
+    case whole
+    case wholeDot
+    case wholeTriplet
+    case half
+    case halfDot
+    case halfTriplet
+    case quarter
+    case quarterDot
+    case quarterTriplet
+    case eight
+    case eightDot
+    case eightTriplet
+    case sixt
+    case sixtDot
+    case sixtTriplet
+    case thirtysecond
+    case thirtysecondDot
+    case thirtysecondTriplet
+    case sixtyfourth
+    case sixtyfourthDot
+    case sixtyfourthTriplet
+    case hundredtwentyeight
+    case hundredtwentyeightDot
+    case hundredtwentyeightTriplet
+}
